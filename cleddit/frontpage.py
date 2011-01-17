ba4bba27460d4713da62885cdcd65d3a0d8b1216
Insikt <in5ikt.github@sekz.se>
2011-01-17 17:25:12 +0000

    Switched from bcolors to colorama, created render() function
    Signed-off-by: Insikt <in5ikt.github@sekz.se>

--- cleddit/frontpage.py
+++ cleddit/frontpage.py
@@ -1,31 +1,39 @@
-import urllib.request, urllib.parse, json, pprint
-from bcolors import bcolors
+import urllib.request, urllib.parse, json, pprint, colorama
 
-def get_frontpage_json():
+from colorama import Fore, Back, Style
+
+colorama.init()
+
+def get_frontpage_json(subreddit = None):
     jsonDecoder = json.JSONDecoder()
-    
-    request = urllib.request.Request('http://www.reddit.com/.json')
+
+    subreddit = 'r/' + subreddit if subreddit else ''
+
+    url = 'http://www.reddit.com/' + subreddit + '.json'
+    request = urllib.request.Request(url)
     
     f = urllib.request.urlopen(request)
     
     return f.read().decode('utf-8')
 
-if __name__ == '__main__':
-    page = json.JSONDecoder().decode( get_frontpage_json() )
+def render(subreddit = None):
+    page = json.JSONDecoder().decode( get_frontpage_json(subreddit) )
     for x in page['data']['children']:
         if type( x ) is dict:
             if x['data']['over_18']:
-                x['data']['title'] = bcolors.ERROR + x['data']['title']
+                x['data']['title'] = Fore.RED + x['data']['title'] + Fore.RESET
             if x['data']['is_self']:
-                print('> {title}\n - {selftext}\n| {url}\n| {author}\n)'.format(title=x['data']['title'],
+                print('> {title}\n - {selftext}\n| {url}\n| {author}\n'.format(title=Style.BRIGHT + x['data']['title'] + Style.RESET_ALL,
                                                                                 author=x['data']['author'],
                                                                                 url=x['data']['url'],
                                                                                 selftext=x['data']['selftext'],
                                                                                 ))
 
             else:
-                print('> {title}\n| {url}\n| {author}\n'.format(title=x['data']['title'], 
+                print('> {title}\n| {url}\n| {author}\n'.format(title=Style.BRIGHT + x['data']['title'] + Style.RESET_ALL,
                                                                 author=x['data']['author'],
                                                                 url=x['data']['url'],
                                                                 ))
 
+if __name__ == '__main__':
+    render()

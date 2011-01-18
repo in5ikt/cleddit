d5451f79bdc6ff45ed46e29d00e1ed95a1eccdbc
Insikt <in5ikt.github@sekz.se>
2011-01-18 13:32:49 +0000

    Added *limit* parameter to *render()* and added docstrings
    Signed-off-by: Insikt <in5ikt.github@sekz.se>

--- cleddit.py
+++ cleddit.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3.1
 from optparse import OptionParser
-import cleddit.frontpage
+import lib.frontpage
 parser = OptionParser()
 
-parser.add_option('-s', '--subreddit', dest='subreddit', help='subreddit')
+parser.add_option('-s', '--subreddit', dest = 'subreddit', help = 'Subreddit, e.g. programming')
+parser.add_option('-l', '--limit', dest = 'limit', help = 'The number of posts you want to be displayed')
 
 (options, args) = parser.parse_args()
 
-cleddit.frontpage.render(options.subreddit if options.subreddit else None)
+lib.frontpage.render(
+    subreddit = options.subreddit if options.subreddit else None, 
+    limit = options.limit if options.limit else None,
+    )
--- lib/frontpage.py
+++ lib/frontpage.py
@@ -1,10 +1,14 @@
-import urllib.request, urllib.parse, json, pprint, colorama
-
+B0;136;0cimport urllib.request, urllib.parse, json, pprint, colorama
 from colorama import Fore, Back, Style
-
 colorama.init()
-
 def get_frontpage_json(subreddit = None):
+    '''
+    Retrieves JSON fron the reddit servers and returns it
+
+    The subreddit parameter states which, if any, subreddit it should fetch.
+    Default is *all*
+    '''
+
     jsonDecoder = json.JSONDecoder()
 
     subreddit = 'r/' + subreddit if subreddit else ''
@@ -16,19 +20,27 @@ def get_frontpage_json(subreddit = None):
     
     return f.read().decode('utf-8')
 
-def render(subreddit = None):
+def render(subreddit = None, limit = 0):
+    '''
+    Renders the list containing title, (selftext)?, url and author
+    '''
+
     page = json.JSONDecoder().decode( get_frontpage_json(subreddit) )
+    rendered_posts = 0
     for x in page['data']['children']:
-        if type( x ) is dict:
+        if type(x) is dict:
             if x['data']['over_18']:
                 x['data']['title'] = Fore.RED + x['data']['title'] + Fore.RESET
             
             print('> {title}\n{selftext}| {url}\n| {author}\n'.format(
-                        title=Style.BRIGHT + x['data']['title'] + Style.RESET_ALL,
+                        title=Style.BRIGHT + x['data']['title'].strip() + Style.RESET_ALL,
                         author=Style.DIM + x['data']['author'] + Style.RESET_ALL,
-                        selftext=' - ' + x['data']['selftext'] if x['data']['is_self'] else '',
+                        selftext=' - ' + x['data']['selftext'].rstrip() + '\n' if x['data']['is_self'] else '',
                         url=x['data']['url'],
                         ))
+            rendered_posts += 1
+            if limit and rendered_posts >= int(limit):
+                break
 
 if __name__ == '__main__':
     render()
